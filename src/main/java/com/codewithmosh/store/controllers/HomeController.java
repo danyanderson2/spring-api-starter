085c6d994615cae008080c1955648685cb683ce8
Dany Anderson <danyanderson2222@gmail.com>
2026-05-28 14:07:52 +0100

    First controller, for home works

---
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.codewithmosh.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  //bean, object managed by spring, this class is henced marked as a bean a spring object
+public class HomeController {
+    @RequestMapping("/")  // maps the root URL to this method, when a user accesses the root URL, this method will be invoked
+    public String index(){
+        return "index.html";
+    }
+}

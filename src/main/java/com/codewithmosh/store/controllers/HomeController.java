787c145161355f6424ed9ee442c84227d89c890f
Dany Anderson <danyanderson2222@gmail.com>
2026-05-29 19:30:10 +0100

    Successfully implemented rendering with thymeleaf and json object data transfer with  the Message class and lombok annotations

--- src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.codewithmosh.store.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller  //bean, object managed by spring, this class is henced marked as a bean a spring object
 public class HomeController {
     @RequestMapping("/")  // maps the root URL to this method, when a user accesses the root URL, this method will be invoked
-    public String index(){
-        return "index.html";
+    public String index(Model model){
+        model.addAttribute("name", "Dany");
+        return "index";
     }
-}
+} 

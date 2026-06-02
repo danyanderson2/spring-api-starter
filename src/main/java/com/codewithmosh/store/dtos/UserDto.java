9eedb98496c9edf39ea19921dd0bd9727ea4f150
Dany Anderson <danyanderson2222@gmail.com>
2026-06-02 10:23:20 +0100

    successfully implemented @JsonIgnore, @JsonInclude, @JsonProperty
    Also implemented the createdAt LocalDateTime

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,12 +1,22 @@
 package com.codewithmosh.store.dtos;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    // @JsonIgnore // exlude id
+    // @JsonProperty("user_id") // rename 
     private Long id;
     private String name;
     private String email;
+    // @JsonInclude(JsonInclude.Include.NON_NULL) // exclude phone number if it's null
+    // private String phoneNumber;
+    private LocalDateTime createdAt;
+
 }

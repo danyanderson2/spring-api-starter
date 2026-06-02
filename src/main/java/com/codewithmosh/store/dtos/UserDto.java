b777771937efcaa19968157654fa2e7cfab85ff4
Dany Anderson <danyanderson2222@gmail.com>
2026-06-02 10:28:07 +0100

    @JsonFormat for date formating applied successfully :)

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,6 +1,7 @@
 package com.codewithmosh.store.dtos;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +18,7 @@ public class UserDto {
     private String email;
     // @JsonInclude(JsonInclude.Include.NON_NULL) // exclude phone number if it's null
     // private String phoneNumber;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // format the date when we serialize it to json
     private LocalDateTime createdAt;
 
 }

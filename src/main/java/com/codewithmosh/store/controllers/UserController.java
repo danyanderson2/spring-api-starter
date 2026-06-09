ee95d2b0c01f6aacf34d18dfed1e309abca092e4
Dany Anderson <danyanderson2222@gmail.com>
2026-06-09 10:37:06 +0100

    implementation of the change-password feature that leverages a special dto for ensuring the old password is known by the instance requesting the change

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -2,8 +2,10 @@ package com.codewithmosh.store.controllers;
 
 import java.util.Set;
 
+import com.codewithmosh.store.dtos.ChangePasswordRequest;
 import com.codewithmosh.store.dtos.RegisterUserRequest;
 import com.codewithmosh.store.dtos.UpdateUserRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Sort;
@@ -66,14 +68,36 @@ public class UserController {
                               @RequestBody UpdateUserRequest request){
         var user = userRepository.findById(id).orElse(null);
         if (user == null){
-            ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().build();
         }
-
         userMapper.update(request,user);
-        assert user != null;
+        userRepository.save(user);
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null){
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(@PathVariable Long id,
+                                               @RequestBody ChangePasswordRequest request){
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null){
+            return ResponseEntity.notFound().build();
+        }
+        if (!user.getPassword().equals(request.getOldPassword())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        user.setPassword(request.getNewPassword());
         userRepository.save(user);
 
-        return ResponseEntity.ok(userMapper.toDto(user));
+        return ResponseEntity.noContent().build(); // note we didn't use a mapper here cuz data is honestly too tiny to require one
 
     }
+
 }
